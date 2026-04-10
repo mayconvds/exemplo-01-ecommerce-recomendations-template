49892f8cd90697bd54b0049486677af3086338e6
Maycon Silva <mayconvds2@gmail.com>
2026-04-10 15:26:21 -0300

    Ajuste da aula 3

--- src/workers/modelTrainingWorker.js
+++ src/workers/modelTrainingWorker.js
@@ -1,14 +1,140 @@
+
 import 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js';
 import { workerEvents } from '../events/constants.js';
+// @ts-ignore
+/** @type {typeof import('@tensorflow/tfjs')} */
+const tf = self.tf;
 
 console.log('Model training worker initialized');
 let _globalCtx = {};
 
 
+const WEIGHTS = {
+    category: 0.4,
+    color: 0.3,
+    price: 0.2,
+    age: 0.1,
+}
+
+const normalize = (value, min, max) => (value - min) / (max - min) || 1;
+
+function makeContext(catalog, users) {
+    const ages = users.map(user => user.age);
+    const prices = catalog.map(product => product.price);
+
+    const minAge = Math.min(...ages);
+    const maxAge = Math.max(...ages);
+
+    const minPrice = Math.min(...prices);
+    const maxPrice = Math.max(...prices);
+
+    const colors = [... new Set(catalog.map(product => product.color))];
+    const categories = [... new Set(catalog.map(product => product.category))];
+
+    const colorsIndex = Object.fromEntries(
+        colors.map((color, index) => {
+            return [color, index]
+        })
+    )
+
+    const categoriesIndex = Object.fromEntries(
+        categories.map((category, index) => {
+            return [category, index]
+        })
+    )
+
+    //computar a média de idade dos compradores por produto
+    // ajuda a personalizar
+
+    const midAge = (minAge + maxAge) / 2;
+    const ageSums = {}
+    const ageCounts = {}
+
+    users.forEach(user => {
+        user.purchases.forEach(product => {
+            ageSums[product.name] = (ageSums[product.name] || 0) + user.age;
+            ageCounts[product.name] = (ageCounts[product.name] || 0) + 1;
+        })
+    })
+
+    const productAvgAgeNorm = Object.fromEntries(
+        catalog.map(product => {
+            const avg = ageCounts[product.name] ? ageSums[product.name] / ageCounts[product.name] : midAge;
+            return [product.name, normalize(avg, minAge, maxAge)];
+        })
+    )
+
+    const numProducts = catalog.length;
+    const numColors = colors.length;
+    const dimentions = 2 + categories.length + numColors;
+    const numCategories = categories.length;
+
+    return {
+        catalog,
+        users,
+        colorsIndex,
+        categoriesIndex,
+        productAvgAgeNorm,
+        minAge,
+        maxAge,
+        minPrice,
+        maxPrice,
+        numCategories: categories.length,
+        numColors: colors.length,
+        // price + age + colors + categories
+        dimentions: 2 + categories.length + colors.length
+    }
+
+}
+
+
+const oneHotWeighted = (index, length, weight) => tf.oneHot(index, length).cast('float32').mul(weight);
+function encodeProduct(product, context) {
+    // normalizando dados para ficar de 0 a 1
+    // e aplicar o peso na recomendação
+    const price = tf.tensor1d([
+        normalize(product.price, context.minPrice, context.maxPrice) * WEIGHTS.price,
+    ]);
+
+    const age = tf.tensor1d([
+        (
+            context.productAvgAgeNorm[product.name] ?? 0.5
+        ) * WEIGHTS.age,
+    ])
+
+    const category = oneHotWeighted(
+        context.categoriesIndex[product.category],
+        context.numCategories,
+        WEIGHTS.category
+    )
+
+    const color = oneHotWeighted(
+        context.colorsIndex[product.color],
+        context.numCategories,
+        WEIGHTS.category
+    )
+
+    return tf.concat([price, age, category, color]);
+}
+
 async function trainModel({ users }) {
     console.log('Training model with users:', users)
 
     postMessage({ type: workerEvents.progressUpdate, progress: { progress: 50 } });
+
+    const catalog = await (await fetch('/data/products.json')).json();
+
+    const context = makeContext(catalog, users);
+    context.productVectores = catalog.map(product => {
+        return {
+            name: product.name,
+            meta: {...product},
+            vector: encodeProduct(product, context).dataSync()
+        }
+    })
+
+    _globalCtx = context;
+
     postMessage({
         type: workerEvents.trainingLog,
         epoch: 1,
